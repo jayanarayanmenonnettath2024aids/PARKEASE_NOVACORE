747449745926b0cbbdfde747305f451b8f9fb6c0
Jayanarayan Menon Nettath <jayanarayanmenonnettath.2024aids@sece.ac.in>
2026-03-04 07:59:22 +0530

    Fix case-sensitive GSAP import for Linux/Vercel

--- frontend_web/src/components/LampUI.jsx
+++ frontend_web/src/components/LampUI.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
-import Draggable from 'gsap/draggable';
+import { Draggable } from 'gsap/all';
 
 gsap.registerPlugin(Draggable);
 
